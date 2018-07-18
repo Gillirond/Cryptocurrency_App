6d9b2ebe6b602a1f1803b4f3e3896c2975998583
Gillirond <bogyn@voliacable.co>
2018-07-18 21:11:13 +0300

    Some new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'crypto-main',
+  template: `<div>
+                <div id="header">IB Cryptocurrency APP</div>
+                <div id="viewer">
+                   <div *ngFor="let item of cryptoData">
+                      <h4>{{item.symbol}} | {{item.name}}</h4>
+                      <h5>24h: {{item.quotes[typeOfCurrency]["percent_change_24h"]}} | 7d: {{item.quotes[typeOfCurrency]["percent_change_7d"]}}</h5>
+                   </div>
+                </div>
+             </div>`,
+  styleUrls: ['./app.component.css'],
+  providers: [ HttpService ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  typeOfCurrency: string = 'USD';
+  cryptoData = {};
+  title = 'IB Crypto APP';
+  constructor (private httpService: HttpService) {}
+
+  ngOnInit() {
+    this.httpService.getData('https://api.coinmarketcap.com/v2/ticker/', function(data) {
+      console.log(data);
+      this.cryptoData = this.objToArrSortedByRank(data.data);
+      console.log(this.cryptoData);
+    }.bind(this));
+  }
+
+  objToArrSortedByRank = function (obj: Object){
+    let arr=[];
+    for(let item in obj) {
+      arr.push(obj[item]);
+    }
+    arr.sort((a,b)=>a["rank"]-b["rank"]);
+    return arr;
+}
 }
