70db008c53a50c492cf13fd772fd13bf466af022
Gillirond <bogyn@voliacable.co>
2018-07-30 23:39:02 +0300

    Added new fixture - when user scrolls to the bottom of the page, new currencies will be uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { HttpService } from './http.service';
+import { DOCUMENT } from "@angular/platform-browser";
+import { WINDOW } from "./window.service";
 
 @Component({
   selector: 'crypto-main',
@@ -7,12 +9,12 @@ import { HttpService } from './http.service';
                 <div id="header">IB Cryptocurrency APP</div>
                 <div id="viewer">
                    <div class="currency-block" *ngFor="let item of cryptoData">
-                      <div class="curr_ico" [ngStyle]="{'background-image':getIcoUrl(item.rank), 'width':'64', 'height':'64'}"></div>
+                      <div [realsrc]="getIcoUrl(item.rank)" class="curr_ico"></div>
                       <div class="curr_name"><b>{{item.symbol}}</b> | {{item.name}}</div> 
                       <div class="curr_price">{{item.quotes[typeOfCurrency]["price"]}} $</div>
                       <div class="clear"></div>
-                      <div class="curr_24h" [ngStyle]="{'color':colorOfPercentDay(item.quotes[typeOfCurrency]['percent_change_24h'])}">24h: <div class="percent">{{item.quotes[typeOfCurrency]["percent_change_24h"]}} %</div></div>
-                      <div class="curr_7d" [ngStyle]="{'color':colorOfPercentWeek(item.quotes[typeOfCurrency]['percent_change_7d'])}">7d: <div class="percent">{{item.quotes[typeOfCurrency]["percent_change_7d"]}} %</div></div>
+                      <div class="curr_24h">24h: <div [colourPercent]="item.quotes[typeOfCurrency]['percent_change_24h']" [koef]="1" class="percent">{{item.quotes[typeOfCurrency]["percent_change_24h"]}} %</div></div>
+                      <div class="curr_7d">7d: <div [colourPercent]="item.quotes[typeOfCurrency]['percent_change_7d']" koef="3" class="percent">{{item.quotes[typeOfCurrency]["percent_change_7d"]}} %</div></div>
                       <div class="clear"></div>
                    </div>
                 </div>
@@ -23,13 +25,28 @@ import { HttpService } from './http.service';
 export class AppComponent implements OnInit{
   typeOfCurrency: string = 'USD';
   cryptoData = [];
-  currenciesCount = 100;
+  currenciesCount = 0;
   title = 'IB Crypto APP';
-  constructor (private httpService: HttpService) {}
+  uploadDelay = false;
 
   ngOnInit() {
-    this.uploadCurrencies.call(this, 1, 100);
+    if(this.uploadCurrencies.call(this, 1, 100))
+      this.currenciesCount+=100;
   }
+
+  constructor(private httpService: HttpService, @Inject(DOCUMENT) private document: Document, @Inject(WINDOW) private window: Window) {}
+
+  @HostListener("window:scroll") loadMoreCurrencies(){
+    let newCurrCount = 25;
+    let listYOffset = this.window.pageYOffset;
+    let scrollHeight = Math.max(this.document.body.scrollHeight, this.document.documentElement.scrollHeight,
+      this.document.body.offsetHeight, this.document.documentElement.offsetHeight,
+      this.document.body.clientHeight, this.document.documentElement.clientHeight);
+    if(scrollHeight - listYOffset < 1000) {
+      if(this.uploadCurrencies.call(this, this.currenciesCount+1, newCurrCount))
+        this.currenciesCount+=newCurrCount;
+    }
+  }.bind(this);
 
   objToArrSortedByRank = function (obj: Object){
     let arr=[];
@@ -38,50 +55,34 @@ export class AppComponent implements OnInit{
     }
     arr.sort((a,b)=>a["rank"]-b["rank"]);
     return arr;
-}
+  };
 
   getIcoUrl = function (rank: String) {
     if(rank!==undefined)
-      return 'url(https://s2.coinmarketcap.com/static/img/coins/64x64/'+rank+'.png';
-  }
+      return 'url(https://s2.coinmarketcap.com/static/img/coins/64x64/'+rank+'.png)';
+  };
 
   uploadCurrencies = function(start: number, limit: number) {
-    if(Number(start)&&Number(limit)&&start>=1&&limit<=100&&limit>=1) {
-      this.httpService.getData('https://api.coinmarketcap.com/v2/ticker/?start='+start+'&limit='+limit, function(data) {
-        this.cryptoData = this.objToArrSortedByRank(data.data);
-        console.log(this.cryptoData);
-      }.bind(this));
+    if(this.uploadDelay == false) {
+      //we need delay to counter function multiple loading
+      this.uploadDelay = true;
+      setTimeout(function() {this.uploadDelay = false}.bind(this), 2000);
+
+      if(Number(start)&&Number(limit)&&start>=1&&limit<=100&&limit>=1) {
+        this.httpService.getData('https://api.coinmarketcap.com/v2/ticker/?start='+start+'&limit='+limit, function(data) {
+          let newCurrs = this.objToArrSortedByRank(data.data);
+          //this.cryptoData.push.apply(this.crypto, newCurrs);
+          for(let i=0;i<newCurrs.length;i++) {
+            this.cryptoData.push(newCurrs[i]);
+          }
+        }.bind(this));
+        return true;
+      }
+      else {
+        console.log("Wrong parameters('start' and 'limit') for uploading currencies");
+        return false;
+      }
     }
-    else
-      console.log("Wrong parameters('start' and 'limit') for uploading currencies");
-
   };
 
-  colorOfPercentDay = function(percent: number) {
-    let colour="";
-    if(percent<=-5)
-      colour="darkred";
-    else if(percent<=-2)
-      colour="orange";
-    else if(percent<=2)
-      colour="black";
-    else if(percent<=5)
-      colour="lightgreen";
-    else colour="green";
-    return colour;
-  };
-
-  colorOfPercentWeek = function(percent: number) {
-    let colour="";
-    if(percent<=-15)
-      colour="darkred";
-    else if(percent<=-6)
-      colour="orange";
-    else if(percent<=6)
-      colour="black";
-    else if(percent<=15)
-      colour="lightgreen";
-    else colour="green";
-    return colour;
-  };
 }
