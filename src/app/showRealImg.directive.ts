9d7d88a0deb0beed49e66b188b493500a0f3eb58
Gillirond <bogyn@voliacable.co>
2018-07-30 23:37:31 +0300

    Created directive which uploads images of currencies only when user see them or scrolled close to them

---
+++ src/app/showRealImg.directive.ts
@@ -0,0 +1,45 @@
+import {Directive, HostListener, ElementRef, Inject, Input, OnInit} from '@angular/core';
+import { DOCUMENT } from "@angular/platform-browser";
+import { WINDOW } from "./window.service";
+
+@Directive({
+  selector: '[realsrc]'
+})
+export class ShowRealImgDirective implements OnInit{
+  @Input("realsrc") imgRealSrc='';
+  private loaded = false;
+
+  /*@HostListener("window:scroll", []) loadIconsAftrScrollEvent() {
+    if(!this.loaded) {
+      if(this.isVisible(this.elementRef.nativeElement)) {
+        this.elementRef.nativeElement.style.backgroundImage = this.imgRealSrc;
+        this.loaded = true;
+        console.log("scrolled");
+      }
+    }
+  };*/
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundImage = this.imgRealSrc;
+  }
+
+  constructor(private elementRef: ElementRef, @Inject(DOCUMENT) private document: Document, @Inject(WINDOW) private window: Window) {
+  }
+
+  isVisible = function(elem) {
+    let coords = elem.getBoundingClientRect();
+    console.log();
+
+    let windowHeight = this.document.documentElement.clientHeight;
+
+    let extendedTop = -2*windowHeight;
+    let extendedBottom = 3 * windowHeight;
+
+    // top visible || bottom visible
+    let topVisible = coords.top > extendedTop && coords.top < extendedBottom;
+    let bottomVisible = coords.bottom < extendedBottom && coords.bottom > extendedTop;
+
+    return topVisible || bottomVisible;
+  };
+
+}
